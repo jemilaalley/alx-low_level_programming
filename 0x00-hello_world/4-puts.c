f6232e36ae9f2deed47d010a1cba3a16a8b48b83
Jemila Alley <jemila.alley@gmail.com>
2022-09-22 14:54:10 -0700

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -Printing
+ *
+ * Return: Always 0 (Successful)
+ */
+int main(void)
+{
+
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+
+}
+
+
