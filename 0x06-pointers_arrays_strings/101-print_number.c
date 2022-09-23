812d16af7f8710bc998ae216e03ad0c1e7503b31
Jemila Alley <jemila.alley@gmail.com>
2022-09-23 08:12:25 +0100

    rewrite code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,19 +9,5 @@
 
 void print_number(int n)
 {
-	unsigned int num;
-	if (n < 0)
-	{
-		num = -n;
-		_putchar('_');
-	}
-	else
-	{
-		num = n;
-	}
-
-	if (num / 10)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
+	_putchar(n);
 }
