b1e1866b425b5d898eb0c4d5a81c601040e89ce8
Jemila Alley <jemila.alley@gmail.com>
2022-09-23 08:33:19 +0100

    added extra files

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -9,5 +9,19 @@
 
 void print_number(int n)
 {
-	_putchar(n);
+	unsigned int num;
+	if (n < 0)
+	{
+		num = -n;
+		_putchar('-');
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
