eded0a0d6f39f79f4cb44d5b0a9d7b78a6564a18
Jemila Alley <jemila.alley@gmail.com>
2022-09-23 06:53:02 +0100

    fix code

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
- * print_number - print numbers chars
- @n: integer params
- *Return: 0
-*/
-
-void print_number (int n)
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
 {
 	unsigned int num = n;
 
